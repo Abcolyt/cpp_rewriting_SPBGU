96682b06d7e34169c86c686dd0d68b12b8a5d77c
Abcolyt <54392414+Abcolyt@users.noreply.github.com>
2024-05-05 19:18:12 +0300

    деление почти работает. добавил разных методов

--- polynomial.h
+++ polynomial.h
@@ -2,6 +2,9 @@
 
 #include<iostream>
 #include <sstream>
+template<typename P> class polynomial;
+template<typename P> std::ostream& operator<<  (std::ostream& out, const polynomial<P>& plnm);
+template<typename P> std::istream& operator>> (std::istream& in, polynomial<P>& plnm);
 template<typename P> class polynomial
 {
 public:
@@ -14,32 +17,68 @@ public:
 	polynomial operator+(const polynomial<P>& other)const;//addition of polynomials
 	polynomial operator-(const polynomial<P>& other)const;
 	polynomial operator/(const polynomial<P>& other)const;//binary division of a polynomial by a polynomial
-	//polynomial operator%(const polynomial<P>& other)const {}//the remainder of the division of a polynomial by a polynomial
+	//polynomial operator%(const polynomial<P>& other)const;//the remainder of the division of a polynomial by a polynomial
 	polynomial operator*(const polynomial<P>& other)const;//binary polynomial multiplication 
 	polynomial operator*(const P& other)const; //binary polynomial multiplication by an element from the field
 
-	//friend std::ostream& operator<<(std::ostream& out, const polynomial<P>& plnm); // overloading the output operator
-	//friend std::istream& operator>>(std::istream& in, polynomial<P>& plnm); // overloading the input operator
+	friend std::ostream& operator<<<>(std::ostream& out, const polynomial<P>& plnm); // overloading the output operator
+	friend std::istream& operator>><>(std::istream& in, polynomial<P>& plnm); // overloading the input operator
+
+	polynomial<P> operator>>(const uint64_t power)const;//coefficient shift (increase). or (plnm*x^n)
+	polynomial<P> operator<<(const uint64_t power)const;//coefficient shift(decrease). or the whole part of (plnm* x^(-n))
 
 	//P& operator[](uint64_t index);//the access operator to the polynomial coefficient
 
 	polynomial<P>& operator=(const polynomial<P>& other);
 
-	bool operator==(const polynomial<P>& other);
-	//bool operator!=(const polynomial& other);
+	bool operator==(const polynomial<P>& other)const;
+	bool operator!=(const polynomial<P>& other)const;
 };
-//no working
-template<typename P>bool polynomial<P>::operator==(const polynomial<P>& other)
+
+template<typename P>polynomial<P> polynomial<P>::operator>>(const uint64_t power)const
 {
-	if (deg != other.deg)return 0;
+	polynomial result(deg + power);
+	for (uint64_t i = 0; i < deg; i++)
+	{
+		result.ptr[i + power] = ptr[i];
+	}
+	return result;
+}
+
+template<typename P>polynomial<P> polynomial<P>::operator<<(const uint64_t power)const
+{
+	polynomial result(deg - power);
+	for (uint64_t i = 0; i <= (deg - power); i++)
+	{
+		result.ptr[i] = ptr[i + power];
+	}
+	return result;
+}
+
+template<typename P>bool polynomial<P>::operator==(const polynomial<P>& other)const //no working //C2666
+{
+	if (deg != other.deg)return false;
 	else
 	{
 		for (uint64_t i = 0; i < deg; i++)
 		{
-			if (ptr[i] != other.ptr[i])return 0;
+			if (ptr[i] != other.ptr[i])return false;
 		}
 	}
-	return 1;
+	return true;
+}
+
+template<typename P>bool polynomial<P>::operator!=(const polynomial<P>& other)const //no working //C2666
+{
+	if (deg != other.deg)return true;
+	else
+	{
+		for (uint64_t i = 0; i < deg; i++)
+		{
+			if (ptr[i] != other.ptr[i])return true;
+		}
+	}
+	return false;
 }
 
 template<typename P>polynomial<P>::polynomial(uint64_t size) {
@@ -92,7 +131,7 @@ template<typename P>std::istream& operator>>(std::istream& in, polynomial<P>& pl
 	return in;
 }
 
-template<typename P> std::ostream& operator<<(std::ostream& out, const polynomial<P>& plnm)
+template<typename P>std::ostream& operator<<(std::ostream& out, const polynomial<P>& plnm)
 {
 	out << "Degree: " << plnm.deg << ", Coefficients: ";
 
@@ -193,10 +232,37 @@ template<typename P>polynomial<P> polynomial<P>::operator*(const P& other)const
 
 }
 //no working
+
 template<typename P>polynomial<P> polynomial<P>::operator/(const polynomial<P>& other)const
 {
+	polynomial<P> result(1);
+	if (deg < other.deg)
+	{
+		result.ptr[0] = 0;
+	}
+	else if ((*this) == other) {
+		result.ptr[0] = 1;
+	}
+	else
+	{
+		result = (*this);
+		uint64_t difference = deg - other.deg;
+		for (int64_t i = difference; i >= 0; i--) {
+			std::cout << "i:" << i << "\n";
+			std::cout << "koef:" << (((result.ptr[other.deg + i - 1] / other.ptr[other.deg - 1]))) << "\n";
+			std::cout << "result.ptr[other.deg + i]" << result.ptr[other.deg + i - 1] << "\n";
+			std::cout << "other >> i" << (other >> i) << "\n";
+			result = result - (other >> i) * (((result.ptr[other.deg + i - 1] / other.ptr[other.deg - 1])));
+			std::cout << "res:" << result << "\n";
+		}
 
+
+	}
+
+	return result;
 }
+
+
 
 #if 0
 int main() {
