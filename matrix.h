694cc0aeb81905319f3aa9308922b09969ce7739
Abcolyt <54392414+Abcolyt@users.noreply.github.com>
2024-06-16 21:53:40 +0300

    +файл с матрицами.h в docx

--- matrix.h
+++ matrix.h
@@ -110,11 +110,6 @@ public:
     //a method that applies a method( output_mode_set(M) ) of class T to each element of the matrix
     //(Not tested. Everything worked in the simplified code) 
     template <typename T, typename ReturnType, typename Param>
-    std::enable_if_t<HasMethodWithParam<T, Param>::value> applyMethodToElements(ReturnType(T::* method)(Param), Param param)const////the implementation is not displayed in the cpp file((
-    {
-        for (size_t i = 0; i < rowsize * colsize; ++i) {
-            (ptr[i].*method)(param);
-        }
-    }
+    std::enable_if_t<HasMethodWithParam<T, Param>::value> applyMethodToElements(ReturnType(T::* method)(Param), Param param)const;
 };
 #include "matrix.cpp"
