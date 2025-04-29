d6cae8dbe3284471758a0796d0c888e0f9addf8e
Ivan Molchanov <ivanmolch2018@mail.ru>
2025-04-29 21:49:33 +0300

    Добавил клавишы: wasd, alt,space,  tab, f1 Для управления, отдаления\приближения, показа текущих настроек отображения, и показа кнопок

--- file_h/Array_xy_To_.h
+++ file_h/Array_xy_To_.h
@@ -5,21 +5,36 @@
 #include <functional>
 #include <cmath>
 
+ 
+
 namespace Drawing_const {};
 using namespace Drawing_const;
 namespace Drawing_const {
-    sf::Vector2u
-        Drawing_Window_Size = { 1600,900 },
-        center_Window = { Drawing_Window_Size.x / 2,Drawing_Window_Size.y / 2 };
     
-    sf::Vector2f ratio_of_modifiers_by_xy = { 16.0f,9.0f },
-        offset_from_Zero_vector = { 0,0 };
-    const double modifire = 5;
-    //vector (-offset_from_Zero_vector) dla sdviga sisemi koordinat
+    //window size
+    sf::Vector2u Drawing_Window_Size = { 1600,900 };
+    //the center of the screen (the center of the graph, that is, the default point (0,0))
+    sf::Vector2u center_Window = { Drawing_Window_Size.x / 2,Drawing_Window_Size.y / 2 };
 
-    const double left_border = (-1) * ((center_Window.x) / (ratio_of_modifiers_by_xy.x * modifire)),
-        right_border = (Drawing_Window_Size.x - center_Window.x) / (ratio_of_modifiers_by_xy.x * modifire),
-        step_by_x = 0.1;
+    //the ratio of relative x and y coordinates
+    sf::Vector2f ratio_of_modifiers_by_xy = { 16.0f,9.0f };
+
+    //the vector of the graph offset from the center of the screen
+    sf::Vector2f  offset_from_Zero_vector = { 0,0 };
+    //scale
+    float modifire = 1;
+
+    
+
+    double left_border  = (-1) * ((center_Window.x) / (ratio_of_modifiers_by_xy.x * modifire)),
+           right_border = (Drawing_Window_Size.x - center_Window.x) / (ratio_of_modifiers_by_xy.x * modifire),
+           step_by_x = 0.1 / modifire;
+    void update_borders() {
+        left_border = (-1) * ((center_Window.x) / (ratio_of_modifiers_by_xy.x * modifire));
+        right_border = (Drawing_Window_Size.x - center_Window.x) / (ratio_of_modifiers_by_xy.x * modifire);
+        step_by_x = 0.1 / modifire;
+    }
+
 }
 
 
@@ -89,27 +104,73 @@ template<class T>void draw_function(T& f) {
 //vector fun object
 template<class T>void draw_functions(const std::vector<std::function<T(T)>>& functions) {
     sf::RenderWindow window(sf::VideoMode(Drawing_Window_Size), "y(x)=");
-    sf::VertexArray axes(sf::PrimitiveType::Lines, 4);
-
-    // OX 
-    axes.append({ sf::Vertex{ sf::Vector2f(0         , center_Window.y), sf::Color::Black } });
-    axes.append({ sf::Vertex{ sf::Vector2f(Drawing_Window_Size.x, center_Window.y), sf::Color::Black } });
-    // OY 
-    axes.append({ sf::Vertex{ sf::Vector2f(center_Window.x ,          0), sf::Color::Black } });
-    axes.append({ sf::Vertex{ sf::Vector2f(center_Window.x , Drawing_Window_Size.y), sf::Color::Black } });
+    
 
     
-    std::vector<sf::VertexArray> graphs;
-    for (const auto& func : functions) {
-        graphs.push_back(funct(func));
-    }
+
     while (window.isOpen()) {
+        //
+        sf::VertexArray axes(sf::PrimitiveType::Lines, 4);
+
+        //Calculating the scales along the X and Y axes (as in the function)
+        const float xScale = ratio_of_modifiers_by_xy.x * modifire;
+        const float yScale = ratio_of_modifiers_by_xy.y * modifire;
+
+        //  OX (y = 0 in relative coordinates)
+        float screenY_OX = center_Window.y - (0 - offset_from_Zero_vector.y) * yScale;
+        axes.append(sf::Vertex(sf::Vector2f(0, screenY_OX), sf::Color::Black));
+        axes.append(sf::Vertex(sf::Vector2f(Drawing_Window_Size.x, screenY_OX), sf::Color::Black));
+
+        //  OY (x = 0 in relative coordinates)
+        float screenX_OY = center_Window.x - (0 - offset_from_Zero_vector.x) * xScale;
+        axes.append(sf::Vertex(sf::Vector2f(screenX_OY, 0), sf::Color::Black));
+        axes.append(sf::Vertex(sf::Vector2f(screenX_OY, Drawing_Window_Size.y), sf::Color::Black));
+        //
+        std::vector<sf::VertexArray> graphs;
+        for (const auto& func : functions) {
+            graphs.push_back(funct(func));
+        }
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>() ||(event->is<sf::Event::KeyPressed>() && event->getIf<sf::Event::KeyPressed>()->code == sf::Keyboard::Key::Escape))
             {
                 window.close();
             }
 
+            if ( event->is<sf::Event::KeyPressed>() ) {
+                #define MAKRO_SHIFT(a,b) Drawing_const::offset_from_Zero_vector = Drawing_const::offset_from_Zero_vector +  sf::Vector2f{ a/modifire,b/modifire };Drawing_const::update_borders();
+                #define MAKRO_ZOOM(k) modifire = modifire * k;Drawing_const::step_by_x*=k;Drawing_const::update_borders();
+                using namespace sf::Keyboard;
+                switch (event->getIf<sf::Event::KeyPressed>()->code)
+                {
+                case Key::Escape:window.close(); break;
+                case Key::A:MAKRO_SHIFT(1,0); break;
+                case Key::D:MAKRO_SHIFT(-1, 0); break;
+                case Key::W:MAKRO_SHIFT(0,1); break;
+                case Key::S:MAKRO_SHIFT(0,-1); break;
+                case Key::Space: MAKRO_ZOOM(3/2); break;
+                case Key::LAlt: MAKRO_ZOOM(2/3); break;
+                case Key::Tab:
+                    std::cout <<"current rendering parameters:\n"
+                              << "l:" << left_border << "\n"
+                              << "r:" << right_border << "\n"
+                              << "m:" << modifire << "\n"
+                              << "s:" << step_by_x << ";\n"; break;
+                case Key::F1:
+                    std::cout << "rendering keys\n"
+                        << "A:MAKRO_SHIFT(1,0)\n"
+                        << "D:MAKRO_SHIFT(-1, 0)\n"
+                        << "W:MAKRO_SHIFT(0,1)\n"
+                        << "S:MAKRO_SHIFT(0,-1)\n"
+                        << "Space: MAKRO_ZOOM(3/2) \n"
+                        << "LAlt: MAKRO_ZOOM(2 / 3) \n"
+                        << "Tab:current rendering parameters (l:left_border ,r:right_border,m:modifire)\n"; break;
+                default:std::cout << "error: there is no fixed behavior\n";
+
+            }
+
+            }
+            
+            
 
 
         }
