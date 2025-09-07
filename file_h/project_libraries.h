8425410b836744251a802b5e18895b497faf7601
Abcolyt <54392414+Abcolyt@users.noreply.github.com>
2025-09-07 13:29:42 +0300

    save. Добавил в папку с заголовками файл третьей части алгоритмов

--- file_h/project_libraries.h
+++ file_h/project_libraries.h
@@ -1,1 +1,28 @@
 #pragma once
+//Standard Libraries
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <functional>
+#include <corecrt_math_defines.h>
+#include <cmath>
+
+//#include <boost/safe_numerics/safe_integer.hpp>
+
+//My Libraries
+#include "file_h/complex.h"
+#include "file_h/fraction.h"
+#include "file_h/polynomial.h"
+#include "file_h/matrix.h"
+
+//My algorithms for libraries
+#include "file_h/counting_methods_1.h"
+#include "file_h/counting_methods_2.h"
+#include "file_h/Array_xy_To_.h"
+#include "file_h/calc_computing_f.h"
+#include "file_h/Spline.h"
+#include "file_h/eigenvalues.h"
+
+#include "file_h/counting_methods_3.h"
