6d192ff9d8b0b0d3a9e6e113198e2da24c025990
Ivan Molchanov <ivanmolch2018@mail.ru>
2025-11-08 12:41:30 +0300

    убрал комменты

--- cpp_rewriting_SPBGU.cpp
+++ cpp_rewriting_SPBGU.cpp
@@ -899,19 +899,7 @@ namespace sem_5 {
 
 int main() {
 
-    // Заголовки таблицы (может быть несколько строк)
-    //std::vector<std::vector<std::string>> headers = {
-    //    {"(n)", "R_Spline_interpolator<3, 2, double>_n","R_Spline_interpolator<3, 2, double>_opt__n","R_teoretical_n/M_n+1", "R_teoretical_opt__n/M_n+1"}
-    //};
-
-    //// Данные таблицы (разные типы в разных столбцах)
-    //std::vector<std::tuple<int, double, double, double, double>> data = {
-    //    {2, 0.426118978341, 0.146720641628, 0.426118978341, 0.146720641628, },
-    //    {2, 0.426118978341, 0.146720641628, 0.426118978341, 0.146720641628, },
-    //    {2, 0.426118978341, 0.146720641628, 0.426118978341, 0.146720641628, }
-    //};
-
-    //sem_5::printTableHeader(headers, data);
+    
 
     //sem_4::sem_4();
     sem_5::sem_5_part1();
