095cd576e61b227934b6621a73db4a0ca9b39ac7
Молчанов Иван Сергеевич <st116628@student.spbu.ru>
2026-03-18 11:50:21 +0300

    новая ветка для перемножения матриц на скорость добавлена, сделал набросок

---
+++ myltiplying_by_speed.cpp
@@ -0,0 +1,19 @@
+#include <../file_h/matrix.h>
+
+template <typename T>
+class derived_matrix : public matrix<T> {
+private:
+	enum class StorageMethod {
+		One_dimensional_array_of_strings,
+		one_dimensional_array_of_columns,
+	};
+	
+
+public:
+
+
+
+
+
+
+}
